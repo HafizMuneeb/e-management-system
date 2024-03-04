58be8937e78cae51a5ef190e20b437673874a73e
Muneeb <muneebafzal381a@gmail.com>
2024-03-04 11:32:14 +0500

    complete login and signup page with front end validations

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,149 @@
-import React from 'react'
+"use client";
 
-const Login = () => {
+import Link from "next/link";
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash, FaUserGraduate } from "react-icons/fa6";
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const isPasswordValid = (): boolean => {
+    // Password should be at least 6 characters
+    return formData.password.length >= 6;
+  };
+
+  const isEmailValid = (): boolean => {
+    // Regular expression for a simple email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(formData.email);
+  };
+
+  const isAnyFieldEmpty = (): boolean => {
+    return Object.values(formData).some((field) => {
+      if (typeof field === "string") {
+        return field.trim() === "";
+      }
+      return false;
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-full h-screen bg-gradient-to-r from-[#24B29A] to-[#199B93] flex justify-center items-center">
+      <div className="w-full md:w-[70%] lg:w-[60%] h-[70%] md:flex mx-auto">
+        {/* image with text */}
+        <div
+          className="w-full md:w-[50%] h-full"
+          style={{
+            backgroundImage: "url('signup.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="w-full h-full flex flex-col justify-center items-center text-white">
+            <h1 className="text-4xl font-bold text-blue-700">Welcome!</h1>
+            <p className="mt-4 text-black">School Management System.</p>
+          </div>
+        </div>
 
-export default Login
+        {/* form div start here */}
+        <div className="w-full md:w-[50%] h-full bg-white p-8 flex flex-col justify-center items-center">
+          <form className="w-full max-w-md">
+            <div className="mb-4 w-full relative">
+              <label
+                htmlFor="identifier"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email
+              </label>
+              <div className="flex items-center">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={`w-full p-2 border rounded ${
+                    isEmailValid() ? "" : "border-red-500"
+                  }`}
+                  onChange={handleChange}
+                  required
+                />
+                {!isEmailValid() && (
+                  <span className="absolute inset-y-1 right-0 pr-2 flex items-center">
+                    <FaUserGraduate className="text-gray-400" />
+                  </span>
+                )}
+              </div>
+              {!isEmailValid() && (
+                <span className="text-red-500">Email is Required</span>
+              )}
+            </div>
+            <div className="mb-4 w-full relative">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Password
+              </label>
+              <div className="flex items-center">
+                <input
+                  type={formData.showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  className={`w-full p-2 border rounded`}
+                  onChange={handleChange}
+                  required
+                />
+                <span
+                  className="absolute inset-y-0 right-0 top-2 pr-2 flex items-center cursor-pointer"
+                  onClick={() =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      showPassword: !prevData.showPassword,
+                    }))
+                  }
+                >
+                  {formData.showPassword ? (
+                    <FaRegEyeSlash className="text-gray-400" />
+                  ) : (
+                    <FaRegEye className="text-gray-400" />
+                  )}
+                </span>
+              </div>
+            </div>
+
+            <div className="mb-4 w-full text-right">
+              <a href="#" className="text-blue-500">
+                Forgot Password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white p-2 rounded w-full"
+              disabled={isAnyFieldEmpty()}
+            >
+              Log In
+            </button>
+          </form>
+          <div className="mt-4 text-gray-700 text-sm">
+            <p>
+              Don't have an account?{" "}
+              <Link href="/signup" className="text-blue-500">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,116 +1,265 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import { useState } from 'react';
-import { useTypewriter } from 'react-simple-typewriter';
-import axios from "axios"
-import { useRouter } from 'next/navigation';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import { IoMdPerson } from "react-icons/io";
+import { FaUserGraduate } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+import Link from "next/link";
 
-const Signup = () => {
-  const router = useRouter();
+const Signup: React.FC = () => {
   const [formData, setFormData] = useState({
     username: "",
     email: "",
-    phone: "",
     password: "",
-    confirmPassword: ""
-  })
+    showPassword: false,
+    confirmPassword: "",
+  });
 
-  const onChangeHandler = (e: any) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setFormData((form) => ({...form, [name]: value}));
-  }
-  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  const onSubmitHandler = async (e: any) => {
-    e.preventDefault();
+  const isUserNameValid = (): boolean => {
+    return formData.username.length >= 6;
+  };
 
-    try {
-      const response = await axios.post("/api/users/signup", formData)
-      console.log(response);
-      
-      toast.success(response.data.msg, {
-        onClose: () => {
-          router.push("/login")
-        }
-      })
+  const isEmailValid = (): boolean => {
+    // Regular expression for a simple email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(formData.email);
+  };
 
-      // router.push("/login")
-    } catch (error) {
-      console.log("Error Occured while creating user");
-      
+  const isPasswordValid = (): boolean => {
+    // Password should be at least 6 characters
+    return formData.password.length >= 6;
+  };
+
+  const getPasswordStrength = (): string => {
+    const { password } = formData;
+
+    const hasLetters = /[a-zA-Z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (password.length >= 8 && hasLetters && hasNumbers && hasSpecialChars) {
+      return "Strong";
+    } else if (password.length >= 6 && (hasLetters || hasNumbers)) {
+      return "Moderate";
+    } else {
+      return "Weak";
     }
-  }
+  };
 
-    const [text] = useTypewriter({
-      words: ['School Management System'],
-      loop: 0
-    })
+  const getPasswordColor = (): string => {
+    const strength = getPasswordStrength();
 
+    switch (strength) {
+      case "Strong":
+        return "text-green-500";
+      case "Moderate":
+        return "text-orange-500";
+      default:
+        return "text-gray-500";
+    }
+  };
+
+  const isPasswordMatch = (): boolean => {
+    return formData.password === formData.confirmPassword;
+  };
+
+  const isAnyFieldEmpty = (): boolean => {
+    return Object.values(formData).some((field) => {
+      if (typeof field === "string") {
+        return field.trim() === "";
+      }
+      return false;
+    });
+  };
 
   return (
-    <div className="w-full h-screen  bg-sky-200 flex justify-center items-center">
-      <div className="w-[400px] h-[520px] shadow-sm shadow-black bg-gray-500 rounded-md flex flex-col justify-center items-center">
-        <div className='flex flex-col justify-center items-center'>
-          <h1 className='text-xl text-[#A6AAE1] font-bold'>Welcome To</h1>
-          <h4 className='text-md text-[#A6AAE1]'>{text}</h4>
+    <div className="w-full h-screen bg-gradient-to-r from-[#24B29A] to-[#199B93] flex justify-center items-center">
+      <div className="w-full md:w-[70%] lg:w-[60%] h-[70%] md:flex mx-auto">
+        {/* image with text */}
+
+        <div
+          className="w-full md:w-[50%] h-full"
+          style={{
+            backgroundImage: "url('signup.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="w-full h-full flex flex-col justify-center items-center text-white">
+            <h1 className="text-4xl font-bold text-blue-700">Welcome!</h1>
+            <p className="mt-4 text-black">School Management System.</p>
+          </div>
         </div>
-        <div className=''>
-          <h1 className='text-xl text-white font-bold'>Sign Up Now</h1>
-        </div>
-        <div className='w-full'>
-          <ToastContainer />
-          <form
-            onSubmit={onSubmitHandler}
-            className='w-full flex flex-col flex-wrap justify-center items-center mt-3 gap-3'
-          >
-            <input type="text" placeholder='User Name' name='username' 
-              onChange={onChangeHandler}
-              value={formData.username}
-              className='w-[80%] text-lg text-blue-950 py-2 rounded-md placeholder:text-blue-600 focus:outline-none focus:ring focus:ring-blue-300'
-            />
-            <input type="email" placeholder='Email' name='email' 
-              onChange={onChangeHandler}
-              value={formData.email}
-              className='w-[80%] text-lg text-blue-950 py-2 rounded-md placeholder:text-blue-600 focus:outline-none focus:ring focus:ring-blue-300'
-            />
-            <input type="password" placeholder='Password' name='password' 
-              onChange={onChangeHandler}
-              value={formData.password}
-              className='w-[80%] text-lg text-blue-950 py-2 rounded-md placeholder:text-blue-600 focus:outline-none focus:ring focus:ring-blue-300'
-            />
-            <input type="number" placeholder='Phone Number' name='phone' 
-              onChange={onChangeHandler}
-              value={formData.phone}
-              className='w-[80%] text-lg text-blue-950 py-2 rounded-md placeholder:text-blue-600 focus:outline-none focus:ring focus:ring-blue-300'
-            />
-            <input type="password" placeholder='Confirm Password' name='confirmPassword' 
-              onChange={onChangeHandler}
-              value={formData.confirmPassword}
-              className='w-[80%] text-lg text-blue-950 py-2 rounded-md placeholder:text-blue-600 focus:outline-none focus:ring focus:ring-blue-300'
-            />
-            
+
+        {/* form div start here */}
+
+        <div className="w-full md:w-[50%] h-full bg-white p-8 flex flex-col justify-center items-center">
+          <form className="w-full max-w-md">
+            <div className="flex flex-col md:flex-row gap-4">
+              <div className="mb-4 w-full relative">
+                <label
+                  htmlFor="username"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  User Name
+                </label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    className={`w-full p-2 border rounded ${
+                      isUserNameValid() ? "" : "border-red-500"
+                    }`}
+                    onChange={handleChange}
+                    required
+                  />
+                  {!isUserNameValid() && (
+                    <span className="absolute inset-y-1 right-0 pr-2 flex items-center">
+                      <FaUserGraduate className="text-gray-400" />
+                    </span>
+                  )}
+                </div>
+                {!isUserNameValid() && (
+                  <span className="text-red-500">User Name is Required</span>
+                )}
+              </div>
+            </div>
+            <div className="mb-2 w-full relative">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email
+              </label>
+              <div className="flex items-center">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="example@gmail.com"
+                  className={`w-full p-2 border rounded ${
+                    isEmailValid() ? "" : "border-red-500"
+                  }`}
+                  onChange={handleChange}
+                  required
+                />
+                {!isEmailValid() && (
+                    <span className="absolute inset-y-1 right-0 pr-2 flex items-center">
+                      <MdEmail className="text-gray-400" />
+                    </span>
+                  )}
+              </div>
+              {!isEmailValid() && (
+                <span className="text-red-500">Email is Required</span>
+              )}
+            </div>
+            <div className="mb-2 w-full relative">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Password
+              </label>
+              <div className="flex items-center">
+                <input
+                  type={formData.showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  className={`w-full p-2 border rounded ${getPasswordColor()}`}
+                  onChange={handleChange}
+                  required
+                />
+                <span
+                  className="absolute inset-y-0 right-0 top-2 pr-2 flex items-center cursor-pointer"
+                  onClick={() =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      showPassword: !prevData.showPassword,
+                    }))
+                  }
+                >
+                  {formData.showPassword ? (
+                    <FaRegEyeSlash className="text-gray-400" />
+                  ) : (
+                    <FaRegEye className="text-gray-400" />
+                  )}
+                </span>
+              </div>
+              <div className={`mb-2 w-full text-sm ${getPasswordColor()}`}>
+                {getPasswordStrength() &&
+                  `Password Strength: ${getPasswordStrength()}`}
+              </div>
+            </div>
+            <div className="mb-2 w-full relative">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Confirm Password
+              </label>
+              <div className="flex items-center">
+                <input
+                  type={formData.showPassword ? "text" : "password"}
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className={`w-full p-2 border rounded ${
+                    isPasswordMatch() ? "border-green-500" : "border-red-500"
+                  }`}
+                  onChange={handleChange}
+                  required
+                />
+                <span
+                  className="absolute inset-y-0 right-0 top-7 pr-2 flex items-center cursor-pointer"
+                  onClick={() =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      showPassword: !prevData.showPassword,
+                    }))
+                  }
+                >
+                  {formData.showPassword ? (
+                    <FaRegEyeSlash className="text-gray-400" />
+                  ) : (
+                    <FaRegEye className="text-gray-400" />
+                  )}
+                </span>
+              </div>
+              {!isPasswordMatch() && (
+                <span className="text-red-500">Passwords do not match</span>
+              )}
+            </div>
+            <div className="mb-2 w-full text-right">
+              <a href="#" className="text-blue-500">
+                Forgot Password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white p-2 rounded w-full"
+              disabled={isAnyFieldEmpty()}
+            >
+              Sign Up
+            </button>
           </form>
-        </div>
-        <div className='flex self-start mt-3 px-10'>
-          <input type="checkbox" />
-          <p className='px-2 text-white text-md'>I agree the terms and conditions</p>
-        </div>
-        <div className='flex self-start px-10'> 
-          <input type="checkbox" />
-          <h1 className='px-2 text-white text-md'> Remeber Me</h1>
-        </div>
-        <button onClick={onSubmitHandler} type='submit' className='bg-blue-500 px-[50px] py-2 text-white text-lg font-bold rounded-full shadow-md shadow-yellow-300'>Submit</button>
-        <div className='flex self-start px-10 mb-3 mt-3'>
-          <h1 className='px-2 text-white text-md'>Already have an account</h1>
-          <Link href="/login" className='text-blue-700 border-b-red-800'>Login Here</Link>
+          <div className="mt-4 text-gray-700 text-sm">
+            <p>
+              Already have an account?{" "}
+              <Link href="/login" className="text-blue-500">
+                Log in
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
